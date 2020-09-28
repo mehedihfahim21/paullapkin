7dc40fcfdfb7a6d7ba7037fdeb308422e0036ec9
mehedihfahim21 <mehedi.hf21@gmail.com>
2020-09-28 16:52:10 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,27 @@
+// MixitUp
+
+var mixer = mixitup('.grid-portfolio-container');
+
+
+// Hamburger Menu
+
+function openMenu() {
+    document.getElementById('navbar').style.height = "100%";
+}
+
+function closeMenu() {
+    document.getElementById('navbar').style.height = "0%";
+}
+
+
+// Smooth Scroll
+
+$(function(){
+    $('.menu-item a, .scroll-down a').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+
+        return false;
+    });
+});
